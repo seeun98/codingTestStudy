ad498a2bda40001c5036d386b99755e055ad601e
junghoon <fpqthel23567@gamil.com>
2022-11-16 17:04:33 +0900

    [프로그래머스42576]완주하지못한선수

--- src/junghoon/완주하지 못한 선수.java
+++ src/junghoon/완주하지 못한 선수.java
@@ -3,23 +3,25 @@ import java.util.Arrays;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-
+        Boolean flag = false;
         Arrays.sort(participant);
         Arrays.sort(completion);
-
+        String an = "";
         System.out.println(Arrays.toString(participant));
         System.out.println(Arrays.toString(completion));
         // ==는 연산자 주소값 비교, .equals 는 값비교
 
-        int i=0;
-        for (i = 0;  i<completion.length; i++) {
-            if (participant[i].equals(completion[i])) {
-                continue;
-            } else {
-                answer= participant[i];
+        for (int i = 0; i < participant.length; i++) {
+            for (int j = 0;  j< participant.length-1; j++) {
+                if (participant[i].equals(completion[j])) {
+                    continue;
+                } else {
+                    an = participant[i];
+                }
+                answer = an;
             }
-
         }
+
         return answer;
 
     }
